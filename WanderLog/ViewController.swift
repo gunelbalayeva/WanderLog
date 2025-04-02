64f37df37038cfff6cead8aa114457698b6edde9
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-04-02 13:07:48 +0400

    Initial Commit

---
+++ WanderLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WanderLog
+//
+//  Created by User on 02.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
